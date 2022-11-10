f1b8ee5bfd4a248fc9ef82057b286082652b1159
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-11-10 23:25:01 +0600

    myreview modified

--- src/page/MyReview/MyReview.js
+++ src/page/MyReview/MyReview.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { baseUrl } from '../../Helper/Helper';
 
 const MyReview = () => {
-    const {name, review} = useLoaderData();
+    const [review, setReview] = useState([]);
+    const {user} = useContext(AuthContext);
+
+    useEffect( () => {
+        fetch(`${baseUrl}/reviews?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => setReview(data))
+    }, [user?.email])
+    
     return (
-        <div className='border border-2 m-4 p-4'>
-            <div className="font-bold">{name}</div>
-            <div className="text-sm opacity-50">{review}</div>
+        <div className="py-20">
+            {
+                review.map(item => {
+                    return (
+                        <div key={item._id} className='border border-2 m-4 p-4'>
+                            <div className="font-bold">{item.email}</div>
+                            <div className="text-sm opacity-50">{item.review}</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
